8cd39e4b743ef414f2fbb2458fb85f9fcd40d420
ZavbiA <anja.zavbiV@gmail.com>
2019-05-30 11:07:34 +0200

    popravki

--- uvoz.R
+++ uvoz.R
@@ -1,39 +1,38 @@
-
 library(readxl)
-#library(dplyr)
+library(dplyr)
 
-
-#Uvoz NA MOJEM KOMPU NI DEDALO read_excel PA SM ZATO V csv SPREMENILA SPODAJ
-#obcine <- read_excel("Seznam.xls")
-#obcine <- obcine[-c(1,2),-c(1,4)]
-#names(obcine) <- c('postna_st', 'posta')
-
-poste <- read.csv2('seznam 2.csv')
-poste <- poste[-c(1,2),-c(1,4)]
-names(poste) <- c('postna_st', 'posta')
-
-#odstranimo nekaj ponavljajočih se post (Lj, Mb, Kp), d alahko uporabimo vekotrske funkcije
+#Najprej uvozimo seznam post.
+poste <- read.csv2(file = "seznam_1.csv")
+#odstranimo nekaj ponavljajocih se post (Lj, Mb, Kp), da lahko uporabimo vektorske funkcije
 poste <- poste[-c(201:240),]
 poste <- poste[-c(273:282),]
 poste <- poste[-c(159:161),]
+colnames(poste) <- c("postna_st", "naziv_poste")
 
+#Nato uvozimo tabelo osebe
 osebe <- read.csv('osebe.csv')
-osebe$prebivalisce <- sample(poste$posta)
 
-#prva baza, samo 1000 podatkov -premalo
-#posiljka <- read.csv('posiljka.csv')
+#Treba je pretvoriti stolpca uporabnisko_ime in telefonska v numeric.
 
-#novo zgenerirana baza, 10 000 podatkov - boljše
+osebe$prebivalisce <- sample(poste$posta) #Premesamo stolpec posta
+
+#Zgeneriramo se tabelo posiljke, 10 000 podatkov.
 posiljke <- read.csv('posiljkee.csv')
 
 #k tabeli posiljke smo dodali posiljatelja in naslovnika, od koder lahko tudi razberemo
 #vstopno in izstopno mesto posiljke
 posiljke$posiljatelj <- osebe$uporabnisko_ime
-posiljke$naslovnik <- sample(osebe$uporabnisko_ime)
+posiljke$naslovnik <- sample(osebe$uporabnisko_ime) #spet premesamo stolpec
+
+#stolpec datum, datum_postaja in datum_prispe moramo pretvorit v date?
 
 #kjer je vmesni datum 0, damo tudi da tudi vmesni kraj 0,
-#saj je pošiljka šla direktno od posiljatelja k naslovniku, ni se vmes ustavljala
-posiljke$vmesni_kraj <- sample(poste$postna_st) 
+#saj je posiljka sla direktno od posiljatelja k naslovniku, ni se vmes ustavljala
+posiljke$vmesni_kraj <- sample(poste$postna_st)
 
+#Naredimo se tabelo za vmesno nahajalisce.
 vmesno_nahajalisce <- posiljke
 vmesno_nahajalisce <- vmesno_nahajalisce[,-c(2,3,5,6,7)]
+
+#Zdaj lahko izbrisemo vmesno postajo iz tabele posiljke.
+posiljke <- posiljke[,-c(4,8)]
