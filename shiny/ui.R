a38d9b5d2571b0f07f4b5b99dac98b8f206e7f54
ajdastare <ajda157@gmail.com>
2019-07-25 20:15:11 +0200

    Dodana slika za shiny

--- shiny/ui.R
+++ shiny/ui.R
@@ -3,21 +3,51 @@ library(shiny)
 
 shinyUI(fluidPage( 
                   
+  vpisniPanel <- tabPanel("SignIn", value="signIn",
+                          fluidPage(
+                            HTML('<body background = "https://raw.githubusercontent.com/ZavbiA/Iskalnik-postnih-posiljk/4b197f8d80fd5b3135ddf0c7ccc31c8fc9f3b492/slike/footer-map-bg.png"></body>'),
+                            fluidRow(
+                              column(width = 12,
+                                     align = "middle",
+                                     textInput("userName","User name", value= "" ),
+                                     passwordInput("password","Password", value = ""),
+                                     actionButton("signin_btn", "Sign In"),
+                                     actionButton("signup_btn", "Sign Up"))
+                    
+                              
+                            ))),
   
-  titlePanel("Brskalnik po postah"),
-
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Iskanje po postah",
-
-             sidebarPanel(
-                uiOutput("izborPoste")
-               ),
-              mainPanel(tableOutput("posiljke")) #iz kere tabele vn pobira
-      )
+  
+registracijaPanel <- tabPanel("SignUp", value = "signUp",
+                                fluidPage(
+                                  fluidRow(
+                                    column(width = 12,
+                                           align="center",
+                                           textInput("SignUpUserName","* Username", value= "", placeholder = "Only Latin characters."),
+                                           passwordInput("SignUpPassword","* Password", value= "", placeholder = "Only Latin characters."),
+                                           actionButton("signup_btnBack", "Back"),
+                                           actionButton("signup_btnSignUp", "Sign Up")
+                                    )
+                                  )
+                                )
   )
-  )
+  
+  
+  
 
-))
+  # mainPanel(
+  #   tabsetPanel(
+  #     tabPanel("Iskanje po postah",
+  # 
+  #            sidebarPanel(
+  #               uiOutput("izborPoste")
+  #              ),
+  #             mainPanel(tableOutput("posiljke")) #iz kere tabele vn pobira
+  #     )
+  # )
+  # )
+
+)
+)
 
 
