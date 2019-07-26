76ea1fccc38029545b754635aac2d6a6783b58c9
ajdastare <ajda157@gmail.com>
2019-07-26 15:30:41 +0200

    Update shiny-a

--- shiny/server.R
+++ shiny/server.R
@@ -28,19 +28,138 @@ shinyServer(function(input, output,session) {
   
 #-------------------------------------------------------------------------------------------------
       
-#ZAVIHEK POSTA
+#Sign in 
   
-  output$izborPoste <- renderUI({
+observeEvent(input$signin_btn,
+               {signInReturn <- sign.in.user(input$userName, input$password)
+               if(signInReturn[[1]]==1){
+                 userID(signInReturn[[2]])
+                 output$signUpBOOL <- eventReactive(input$signin_btn, 2)
+                 loggedIn(TRUE)
+               }else if(signInReturn[[1]]==0){
+                 showModal(modalDialog(
+                   title = "Error during sign in",
+                   paste0("Error occured. Please try again."),
+                   easyClose = TRUE,
+                   footer = TRUE
+                 ))
+               }else{
+                 showModal(modalDialog(
+                   title = "Wrong Username/Password",
+                   paste0("Username or/and password incorrect"),
+                   easyClose = TRUE,
+                   footer = NULL
+                 ))
+               }
+               })
+  
+# Sign in function
+sign.in.user <- function(username, pass){
+    # Return a list. In the first place is an indicator of success:
+    # 1 ... success
+    # 0 ... error
+    # -10 ... wrong username
+    # The second place represents the userid if the login info is correct,
+    # otherwise it's NULL
+    success <- 0
+    uporabnikID <- NULL
+    tryCatch({
+      drv <- dbDriver("PostgreSQL")
+      conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+      userTable <- tbl(conn, "uporabnik")
+      obstoj <- 0
+      # obstoj = 0, ce username in geslo ne obstajata,  1 ce obstaja
+      uporabnik <- username
+      geslo <- pass
+      hashGesla <- (userTable %>% filter(username == uporabnik) %>% collect() %>% pull(hash))[[1]]
+      if(checkpw(geslo, hashGesla)){
+        obstoj <- 1
+      }
+      if(obstoj == 0){
+        success <- -10
+      }else{
+        uporabnikID <- (userTable %>% filter(username == uporabnik) %>%
+                          collect() %>% pull(id))[[1]]
+        success <- 1
+      }
+    },warning = function(w){
+      print(w)
+    },error = function(e){
+      print(e)
+    }, finally = {
+      dbDisconnect(conn)
+      return(list(success, uporabnikID))
+    })
+  }
+  
+  pridobi.ime.uporabnika <- function(userID){
+    # Pridobi ime vpisanega glede na userID
+    tryCatch({
+      drv <- dbDriver("PostgreSQL")
+      conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+      sqlInput<- build_sql("SELECT uporabnisko_ime FROM osebe WHERE id=",userID, con = conn)
+      userid <- dbGetQuery(conn, sqlInput)
+    },finally = {
+      dbDisconnect(conn)
+      return(unname(unlist(userid)))
+    }
+    )
+  }
+  
+  probi.upor.ime.geslo <- function(userName, password){
+    tryCatch({
+      drv <- dbDriver("PostgreSQL")
+      conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+      sqlInput<- build_sql("SELECT uporabnisko_ime FROM osebe WHERE id=",userName, con = conn)
+      userid <- dbGetQuery(conn, sqlInput)
+      
+    },finally = {
+      dbDisconnect(conn)
+      return(unname(unlist(userid)))
+    }
+    )
     
-    izbira_poste=dbGetQuery(conn, build_sql("SELECT posiljatelj FROM posiljke"))
-    Encoding(izbira_poste[,1])="UTF-8" #vsakic posebej je potrebno character stolpce spremeniti v pravi encoding.
+  }
   
-    selectInput("posiljatelj",
-                            label = "Izberite posto:",
-                            choices = izbira_poste,
-                            selected = "129912621" #bo vsaj tti pošiljatttelj izbran
-                )
-  })
+  
+
+
+  
+  
+  
+  
+  # Login/logout button in header
+  # observeEvent(input$dashboardLogin, {
+  #   if(loggedIn()){
+  #     output$signUpBOOL <- eventReactive(input$signin_btn, 0)
+  #     userID <- reactiveVal()
+  #   }
+  #   loggedIn(ifelse(loggedIn(), FALSE, TRUE))
+  # })
+  # 
+  # output$logintext <- renderText({
+  #   if(loggedIn()) return("Logout here.")
+  #   return("Login here")
+  # })
+  # 
+  # output$dashboardLoggedUser <- renderText({
+  #   if(loggedIn()) return(paste("Welcome,", pridobi.ime.uporabnika(userID())))
+  #   return("")
+  # })
+  # 
+  
+  
+  # output$izborPoste <- renderUI({
+  #   
+  #   izbira_poste=dbGetQuery(conn, build_sql("SELECT posiljatelj FROM posiljke"))
+  #   Encoding(izbira_poste[,1])="UTF-8" #vsakic posebej je potrebno character stolpce spremeniti v pravi encoding.
+  # 
+  #   selectInput("posiljatelj",
+  #                           label = "Izberite posto:",
+  #                           choices = izbira_poste,
+  #                           selected = "129912621" #bo vsaj tti pošiljatttelj izbran
+  #               )
+  # })
 
 
   
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,40 +1,86 @@
 library(shiny)
+library(shinydashboard)
+source("server.R")
 
 
-shinyUI(fluidPage( 
-                  
-  vpisniPanel <- tabPanel("SignIn", value="signIn",
-                          fluidPage(
-                            HTML('<body background = "https://raw.githubusercontent.com/ZavbiA/Iskalnik-postnih-posiljk/master/slike/digital-mail-2-1.jpg"></body>'),
-                            fluidRow(
-                              column(width = 12,
-                                     align = "center",
-                                     textInput("userName","User name", value= "", placeholder = "User name"),
-                                     passwordInput("password","Password", value = "", placeholder = "Password"),
-                                     actionButton("signin_btn", "Sign In"),
-                                     actionButton("signup_btn", "Sign Up"))
-                    
-                              
-                            ))),
-  
-  
-registracijaPanel <- tabPanel("SignUp", value = "signUp",
-                                fluidPage(
-                                  fluidRow(
-                                    column(width = 12,
-                                           align="center",
-                                           textInput("SignUpUserName","* Username", value= "", placeholder = "Only Latin characters."),
-                                           passwordInput("SignUpPassword","* Password", value= "", placeholder = "Only Latin characters."),
-                                           actionButton("signup_btnBack", "Back"),
-                                           actionButton("signup_btnSignUp", "Sign Up")
-                                    )
-                                  )
-                                )
+library(shiny)
+library(shinydashboard)
+
+
+vpisniPanel <- tabPanel("SignIn", value="signIn",
+                        fluidPage(
+                          HTML('<body background = "https://raw.githubusercontent.com/ZavbiA/Iskalnik-postnih-posiljk/master/slike/digital-mail-2-1.jpg"></body>'),
+                          fluidRow(
+                            column(width = 12,
+                                   align = "middle",
+                                   textInput("userName","User name", value= "", placeholder = "User name"),
+                                   passwordInput("password","Password", value = "", placeholder = "Password"),
+                                   actionButton("signin_btn", "Sign In")
+                            )
+                            
+                            
+                          )))
+
+
+
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title="Pošta FMF"),
+    dashboardSidebar(
+      menuItem("Pomoč uporabnikom",tabName = "pomoc"),
+      menuItem("Navodila",tabName = "navodila")),
+    dashboardBody(vpisniPanel),
+    conditionalPanel(condition = "output.signUpBOOL=='1'",
+                     # Panel, ko je bila uspesna registracija
+                     titlePanel("Sledenje pošiljke"))
+                       
+
+    )
   )
-  
 
 
-  
+
+
+
+
+
+
+
+
+ 
+
+# body<-mainPanel(
+#   tabItems(
+#     tabItem(tabName = "Sledenje posiljke",
+#             fluidRow(sidebarPanel(
+#               uiOutput("")
+#             )))
+#     ,
+#     tabItem(tabName = "Pomoc uporabnikom")
+#            
+# )
+# )
+# 
+#   
+# fluidPage(useShinyjs(),
+#           conditionalPanel(condition = "output.signInBOOL!='0' && output.signInBOOL!='-10'",#&& false", 
+#                            vpisniPanel),       # UI panel za vpis
+#           conditionalPanel(condition = "output.signUpBOOL=='1'",
+#                            # Panel, ko je bila uspesna registracija
+#                            titlePnael("Sledenje pošiljke"),
+#                            sidebarLayout(
+#                              sidebarPanel(
+#                                actionButton("pomoc", "Pomoc Uporabnikom"),
+#                                width = 4
+#                                
+#                              )),
+#                            mainPanel()
+#                            
+#                            ),
+#           theme="bootstrap.css"
+# )
+# 
+#   
 
   # mainPanel(
   #   tabsetPanel(
@@ -48,7 +94,5 @@ registracijaPanel <- tabPanel("SignUp", value = "signUp",
   # )
   # )
 
-)
-)
 
 
