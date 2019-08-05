dae0fdd87ead65c903cf0e8f59d0a30ebceb7384
ZavbiA <anja.zavbiV@gmail.com>
2019-08-05 20:34:49 +0200

    premaknjen uvoz

---
+++ uvoz.R
@@ -0,0 +1,54 @@
+library(readxl)
+library(dplyr)
+library(readr)
+library(digest)
+
+#Najprej uvozimo seznam post.
+poste <- read.csv2(file = "seznam_1.csv")
+#odstranimo nekaj ponavljajocih se post (Lj, Mb, Kp), da lahko uporabimo vektorske funkcije
+poste <- poste[-c(201:240),]
+poste <- poste[-c(273:282),]
+poste <- poste[-c(159:161),]
+colnames(poste) <- c("postna_stevilka", "naziv_poste")
+
+#Nato uvozimo tabelo osebe
+osebe <- read.csv('osebe.csv')
+#Pretvorimo stolpca uporabnisko_ime in telefonska v tip numeric:
+osebe$uporabnisko_ime <- gsub("-", "", osebe$uporabnisko_ime) %>% parse_number()
+# osebe$uporabnisko_ime <- toString(osebe$uporabnisko_ime)
+osebe$telefonska <- gsub("-", "", osebe$telefonska) %>% parse_number()
+
+#Stolpec geslo spremenimo, da to niso vec prava gesla, ampak hash.
+osebe$geslo <- sapply(osebe$geslo, digest, algo="md5")
+
+#Dodamo in premesamo stolpec prebivalisce.
+osebe$prebivalisce <- sample(poste$naziv_poste)
+#Ta vrstica nekaj naredi, da to ni vec tabela?
+is.table(osebe)
+
+#Zgeneriramo se tabelo posiljke, 10 000 podatkov.
+posiljke <- read.csv('posiljkee.csv')
+
+#Stolpca datum_postaja in datum_prispe pretvorimo v tip date:
+posiljke$datum_oddaje <- as.character(posiljke$datum_oddaje)
+posiljke$datum_prispe <- as.character(posiljke$datum_prispe)
+posiljke$vmesna_postaja <- as.character(posiljke$vmesna_postaja)
+posiljke$datum_oddaje <- parse_date(posiljke$datum_oddaje, format = "%m/%d/%Y")
+posiljke$datum_prispe <- parse_datetime(posiljke$datum_prispe, format = "%m/%d/%Y %I:%M %p")
+posiljke$vmesni_datum <- parse_datetime(posiljke$vmesna_postaja, format = "%m/%d/%Y %I:%M %p")
+
+#K tabeli posiljke smo dodale posiljatelja in naslovnika, od koder lahko tudi
+#razberemo vstopno in izstopno mesto posiljke.
+posiljke$posiljatelj <- osebe$uporabnisko_ime
+posiljke$naslovnik <- sample(osebe$uporabnisko_ime) #spet premesamo stolpec
+
+#Kjer je vmesni datum 0, damo tudi da tudi vmesni kraj 0,
+#saj je posiljka sla direktno od posiljatelja k naslovniku, ni se vmes ustavljala
+posiljke$vmesni_kraj <- sample(poste$postna_stevilka)
+
+#Naredimo se tabelo za vmesno nahajalisce.
+vmesno_nahajalisce <- posiljke
+vmesno_nahajalisce <- vmesno_nahajalisce[,-c(2,3,4,6)]
+
+#Zdaj lahko izbrisemo vmesno postajo iz tabele posiljke.
+posiljke <- posiljke[,-c(4,8)]
