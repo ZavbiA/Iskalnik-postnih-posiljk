80354a3ea47dc38e2ca63fa2f3017b61c164894c
ZavbiA <anja.zavbiV@gmail.com>
2019-08-14 10:57:08 +0200

    server source public

--- shiny/server.R
+++ shiny/server.R
@@ -9,8 +9,7 @@ library(digest)
 #ČE TI KDAJ NAPISE  DA SI PRESEGEL MAX POVEZAV, ZAZENI TO:
 #RPostgreSQL::dbDisconnect(RPostgreSQL::dbListConnections(RPostgreSQL::PostgreSQL())[[1]]) 
 
-#source("auth.R") #skopiraj svojo auth.R datoteko v mapo shiny
-#source("auth_public.R")
+source("auth_public.R")
 
 #Za probleme s sumniki uporabi:
 original.locale <- Sys.getlocale(category="LC_CTYPE")       ## "English_Slovenia.1252" pri meni
